5d916f45ac97196f3b4f053a940649ef8f4d7654
Riccardo Emanuele Todaro <rtodaro@e4r10p2.42roma.it>
2025-04-28 18:58:59 +0200

    init ex01

---
+++ ex01/Contact.hpp
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Contact.hpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rtodaro <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/28 17:41:31 by rtodaro           #+#    #+#             */
+/*   Updated: 2025/04/28 18:57:21 by rtodaro          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <string>
+
+class Contact
+{
+	private:
+		std::string	first_name;
+		std::string	last_name;
+		std::string nickname;
+		std::string	phone_number;
+		std::string	firstname;
+		std::string	darkest_secret;
+
+	public: 
+
+}
